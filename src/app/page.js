5c94b2046ff77c6eba1dc25e6c86b0cefb1bff1a
Najibmansouur <najibmansour2005@gmail.com>
2023-04-27 03:59:57 +0300

    rick roll

--- src/app/page.js
+++ src/app/page.js
@@ -12,22 +12,20 @@ const Home = () => {
           <ExchangeTable />
         </div>
         <div className="card lg:w-full lg:h-full bg-base-300 p-3 lg:mt-0 mt-14">
-          Eum magnam qui ea ipsam voluptas placeat aut. Sint quod temporibus
-          est. Minus commodi quae illo laudantium dicta doloribus tenetur et.
-          Esse ut est. Aut earum voluptatum mollitia esse et similique veniam ut
-          fugit. Aperiam accusamus harum expedita corrupti maxime. Tempore
-          <br />
-          <br />
-          tempora eaque qui mollitia vel nihil officiis. Eum atque saepe
-          excepturi nihil magnam fuga beatae occaecati. Facilis quam suscipit
-          <br />
-          <br />
-          dolores commodi omnis omnis velit aut. Rerum eligendi adipisci
-          <br />
-          <br />
-          reprehenderit quis quos omnis laboriosam quis occaecati. Quia autem
-          eum occaecati cumque quis necessitatibus officia dolorem dolorem. Hic
-          porro at dolorum enim aspernatur nisi autem maiores.
+          Never gonna give you up Never gonna let you down Never gonna run
+          around and desert you Never gonna make you cry Never gonna say goodbye
+          Never gonna tell a lie and hurt you We've known each other for so long
+          Your heart's been aching, but you're too shy to say it (to say it)
+          Inside, we both know what's been going on (going on) We know the game
+          and we're gonna play it I just wanna tell you how I'm feeling Gotta
+          make you understand Never gonna give you up Never gonna let you down
+          Never gonna run around and desert you Never gonna make you cry Never
+          gonna say goodbye Never gonna tell a lie and hurt you Never gonna give
+          you up Never gonna let you down Never gonna run around and desert you
+          Never gonna make you cry Never gonna say goodbye Never gonna tell a
+          lie and hurt you Never gonna give you up Never gonna let you down
+          Never gonna run around and desert you Never gonna make you cry Never
+          gonna say goodbye Never gonna tell a lie and hurt you
         </div>
       </div>
     </div>
