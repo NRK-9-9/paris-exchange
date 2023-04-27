fe65a7f3d972b2bde8719140e3f247d1208465c2
Najibmansouur <najibmansour2005@gmail.com>
2023-04-28 02:54:53 +0300

    fix rikrol

--- src/app/page.js
+++ src/app/page.js
@@ -14,18 +14,19 @@ const Home = () => {
         <div className="card lg:w-full lg:h-full bg-base-300 p-3 lg:mt-0 mt-14">
           Never gonna give you up Never gonna let you down Never gonna run
           around and desert you Never gonna make you cry Never gonna say goodbye
-          Never gonna tell a lie and hurt you We've known each other for so long
-          Your heart's been aching, but you're too shy to say it (to say it)
-          Inside, we both know what's been going on (going on) We know the game
-          and we're gonna play it I just wanna tell you how I'm feeling Gotta
-          make you understand Never gonna give you up Never gonna let you down
-          Never gonna run around and desert you Never gonna make you cry Never
-          gonna say goodbye Never gonna tell a lie and hurt you Never gonna give
-          you up Never gonna let you down Never gonna run around and desert you
-          Never gonna make you cry Never gonna say goodbye Never gonna tell a
-          lie and hurt you Never gonna give you up Never gonna let you down
-          Never gonna run around and desert you Never gonna make you cry Never
-          gonna say goodbye Never gonna tell a lie and hurt you
+          Never gonna tell a lie and hurt you W&apos;ve known each other for so
+          long Your heart&apos;s been aching, but you&apos;re too shy to say it
+          (to say it) Inside, we both know what&apos;s been going on (going on)
+          We know the game and we&apos;re gonna play it I just wanna tell you
+          how I&apos;m feeling Gotta make you understand Never gonna give you up
+          Never gonna let you down Never gonna run around and desert you Never
+          gonna make you cry Never gonna say goodbye Never gonna tell a lie and
+          hurt you Never gonna give you up Never gonna let you down Never gonna
+          run around and desert you Never gonna make you cry Never gonna say
+          goodbye Never gonna tell a lie and hurt you Never gonna give you up
+          Never gonna let you down Never gonna run around and desert you Never
+          gonna make you cry Never gonna say goodbye Never gonna tell a lie and
+          hurt you
         </div>
       </div>
     </div>
