2da10d9210590c48a726105794dc4f98d20bf21b
Najibmansouur <najibmansour2005@gmail.com>
2023-05-11 23:41:20 +0300

    asdasd

--- src/components/metalPrice.js
+++ src/components/metalPrice.js
@@ -6,13 +6,13 @@ import Image from "next/image";
 const MetalPrice = ({ gold }) => {
   return (
     <div className="p-3">
-      <div className="flex justify-center mb-3">
-        <h1 className="font-semibold text-3xl animate-pulse-blackred">
-          Prix Or du jour:
-        </h1>
-      </div>
       <div className="card rounded-t-md bg-white mt-6 shadow-lg">
-        <div className="flex justify-center place-items-center m-3 mt-5">
+        <div className="flex flex-col justify-center place-items-center m-3 mt-5">
+          <div className="flex justify-center mb-3">
+            <h1 className="font-semibold text-3xl animate-pulse-blackred">
+              Prix Or du jour:
+            </h1>
+          </div>
           <Image src={goldpng} width={150} height={150} alt="gold_png"></Image>
         </div>
         <div className="bg-neutral-800 p-4 text-neutral-content rounded-b-md">
