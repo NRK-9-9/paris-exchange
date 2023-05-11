39291ea20206bd2ddde9ade3b898c84e90d2482c
Najibmansouur <najibmansour2005@gmail.com>
2023-05-11 23:29:09 +0300

    flashing red black

--- src/components/metalPrice.js
+++ src/components/metalPrice.js
@@ -7,14 +7,16 @@ const MetalPrice = ({ gold }) => {
   return (
     <div className="p-3">
       <div className="flex justify-center mb-3">
-        <h1 className="font-semibold text-3xl">Prix Or du jour:</h1>
+        <h1 className="font-semibold text-3xl animate-pulse-blackred">
+          Prix Or du jour:
+        </h1>
       </div>
-      <div className="card rounded-t-md bg-white mt-6">
+      <div className="card rounded-t-md bg-white mt-6 shadow-lg">
         <div className="flex justify-center place-items-center m-3 mt-5">
           <Image src={goldpng} width={150} height={150} alt="gold_png"></Image>
         </div>
         <div className="bg-neutral-800 p-4 text-neutral-content rounded-b-md">
-          <p className="flex justify-center text-xl font-semibold">
+          <p className="flex justify-center text-2xl font-semibold">
             Nous Achetons:
           </p>
           <div className="flex flex-row mt-2 text-lg ">
