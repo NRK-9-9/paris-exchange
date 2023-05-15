ad639188795d540273e2fd2aee2329ef2e8d5f9a
Najibmansouur <najibmansour2005@gmail.com>
2023-05-16 01:52:35 +0300

    table!

--- src/components/table.js
+++ src/components/table.js
@@ -6,16 +6,16 @@ export default function Table({ exchangeData, type, selectedCountry }) {
   console.log(selectedCountry);
 
   return (
-    <table className="mt-5">
+    <table className="mt-5 lg:w-[30vw] w-min">
       <thead className="">
         <tr>
-          <th className="bg-neutral lg:text-sm text-xs text-neutral-content px-6 py-3 rounded-tl-lg">
+          <th className="bg-neutral lg:text-xs text-xs text-neutral-content px-6 py-3 rounded-tl-lg">
             Pays
           </th>
-          <th className="bg-neutral lg:text-sm text-xs text-neutral-content px-6 py-3">
+          <th className="bg-neutral lg:text-xs text-xs text-neutral-content px-6 py-3">
             {type === "sell" ? "PARS EXCHANGE VEND" : "PARS EXCHANGE ACHETE"}
           </th>
-          <th className="bg-neutral lg:text-sm text-xs text-neutral-content px-6 py-3 rounded-tr-lg">
+          <th className="bg-neutral lg:text-xs text-xs text-neutral-content px-6 py-3 rounded-tr-lg">
             {type === "sell" ? "PE VEND" : "PE ACHETE"}
           </th>
         </tr>
