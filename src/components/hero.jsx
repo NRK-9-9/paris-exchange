be1d45a8d7fa0accac12331ae7a7360d5727280f
Najibmansouur <najibmansour2005@gmail.com>
2023-12-10 02:58:29 +0200

    alah mayyit

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-2xl">
-            <h2 className="mb-5 lg:text-5xl text-xl font-bold font-logoFont tracking-wide">
+            <h2 className="lg:mb-5 mb-2 lg:text-5xl text-xl font-bold font-logoFont tracking-wide">
               Bureau De Change
             </h2>
             <p className="tracking-wide lg:text-lg text-xs">
