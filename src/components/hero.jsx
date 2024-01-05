5c71b9e79d42fba6035baaddd4ccb17157d216fd
Najibmansouur <najibmansour2005@gmail.com>
2024-01-06 01:51:39 +0200

    hero img

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,11 +2,12 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="flex justify-center ">
-      <div className="hero  lg:min-h-[40vh] min-h-[15vh] bg-[url('https://i.imgur.com/Bq2sNWj.jpeg')]">
+    <div className="flex justify-center">
+      <div className="hero  lg:min-h-[40vh] min-h-[15vh] bg-[url('https://i.imgur.com/lEg3fIw.jpg')]">
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center text-neutral-content">
-          <div className="max-w-2xl">
+          <div className="flex flex-col justify-end"></div>
+          <div className="max-w-2xl ">
             <h2 className="lg:mb-5 mb-2 lg:text-5xl text-xl font-bold font-logoFont tracking-wide">
               Bureau De Change
             </h2>
