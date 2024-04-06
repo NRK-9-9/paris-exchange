ce9c2406e39f83d6b6b7830035f17a12eea297f4
Najibmansouur <najibmansour2005@gmail.com>
2024-04-06 22:36:57 +0300

    Added google reviews widget

--- src/app/(website)/page.js
+++ src/app/(website)/page.js
@@ -2,6 +2,7 @@ import Hero from "@/components/hero";
 import IndexPage from "@/components/indexPage";
 import { ReviewCarrousel } from "@/components/reviewCaroussel";
 import Sentence from "@/components/sentence";
+import Script from "next/script";
 import React from "react";
 
 const Home = () => {
@@ -10,8 +11,22 @@ const Home = () => {
       <Hero />
       <Sentence />
       <IndexPage></IndexPage>
-      <div className="w-[95vw] flex justify-center items-center">
-        <ReviewCarrousel />
+      <div className=" lg:mt-10 px-7 h-80 ">
+        {/* <iframe
+          src="https://1ab78ef2c13e49e2a78daf6af8b43070.elf.site"
+          width="100%"
+          height="100%"
+          frameborder="0"
+        ></iframe> */}
+        <Script
+          src="https://static.elfsight.com/platform/platform.js"
+          data-use-service-core
+          defer
+        ></Script>
+        <div
+          class="elfsight-app-1ab78ef2-c13e-49e2-a78d-af6af8b43070"
+          data-elfsight-app-lazy
+        ></div>
       </div>
     </main>
   );
